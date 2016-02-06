e05481b9747c19db7346d3f72bc230caba8cf2aa
Kai Grossjohann <kai.grossjohann@epochtimes.de>
2016-02-06 13:48:28 +0100

    Allow multiple prefix keys.
    Because multiple prefix keys may use the same suffix key, I cannot bind
    the suffix key when establishing the keybinding, for that would result
    in the same suffix key being bound twice.
    
    So now when adding suffixes I just remember what needs to be added.  And
    then, when the prefix key is hit, I bind all the suffix keys right then
    and there.  And once after a suffix key has been hit, I remove the
    bindings again that were established when the prefix key was hit.

--- phoenix.js
+++ phoenix.js
@@ -34,6 +34,7 @@ function move_window(rect) {
 function PrefixKey(key, modifiers, description) {
     this.modal = centered_modal(description);
     this.suffixes = [];
+    this.handlers = [];
     var that = this;
     this.prefix = Phoenix.bind(key, modifiers, function () {
         that.enableSuffixes();
@@ -42,44 +43,46 @@ function PrefixKey(key, modifiers, description) {
 }
 
 PrefixKey.prototype.enableSuffixes = function () {
+    var that = this;
     this.suffixes.forEach(function (x) {
-        x.enable();
+        var h = Phoenix.bind(x.key, x.modifiers, function () {
+            x.cb();
+            that.disableSuffixes();
+            that.modal.close();
+        });
+        h.enable();
     });
 };
 PrefixKey.prototype.disableSuffixes = function () {
-    this.suffixes.forEach(function (x) {
+    this.handlers.forEach( function (x) {
         x.disable();
     });
+    this.handlers = [];
 };
 PrefixKey.prototype.addSuffix = function (key, modifiers, cb) {
-    var that = this;
-    var result = Phoenix.bind(key, modifiers, function () {
-        cb();
-        that.modal.close();
-        that.disableSuffixes();
-    });
-    result.disable();
-    this.suffixes.push(result);
-    return result;
+    this.suffixes.push({key: key, modifiers: modifiers, cb: cb});
 };
 
 /* Window handling prefix key */
 
 var wPrefix = new PrefixKey('space', ['alt'], "h - Left Half\nl - Right Half\ng - Wide Center\nm - Max\nesc - Abort");
-var wLeft = wPrefix.addSuffix('h', [], function () {
+wPrefix.addSuffix('h', [], function () {
     move_window({x: 0, y: 0, width: 0.5, height: 1.0});
 });
-var wRight = wPrefix.addSuffix('l', [], function () {
+wPrefix.addSuffix('l', [], function () {
     move_window({x: 0.5, y: 0, width: 0.5, height: 1.0});
 });
-var wCenter = wPrefix.addSuffix('g', [], function () {
+wPrefix.addSuffix('g', [], function () {
     move_window({x: 0.15, y: 0, width: 0.7, height: 1.0});
 });
-var wMax = wPrefix.addSuffix('m', [], function () {
+wPrefix.addSuffix('m', [], function () {
     move_window({x: 0, y: 0, width: 1.0, height: 1.0});
 });
-var wAlmostMax = wPrefix.addSuffix('n', [], function () {
+wPrefix.addSuffix('n', [], function () {
     move_window({x: 0.1, y:0, width: 0.9, height: 1.0});
 });
-var wEscape = wPrefix.addSuffix('escape', [], function () {});
+wPrefix.addSuffix('c', [], function () {
+    move_window({x: 0.2, y: 0.2, width: 0.6, height: 0.6});
+});
+wPrefix.addSuffix('escape', [], function () {});
 
