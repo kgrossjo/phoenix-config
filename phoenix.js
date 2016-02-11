b821b652a756274df00a248269fb62ed784ff0c3
Kai Grossjohann <kai.grossjohann@epochtimes.de>
2016-02-11 12:20:38 +0100

    Support moving the current window to the next screen.

--- phoenix.js
+++ phoenix.js
@@ -18,8 +18,9 @@ var h_reload = Phoenix.bind('r', ['alt'], function () {
 
 /* Window Handling */
 
-function move_window(rect) {
-    var scr = Screen.mainScreen().visibleFrameInRectangle();
+function move_window(rect, screen) {
+    screen = screen || Screen.mainScreen();
+    var scr = screen.visibleFrameInRectangle();
     var r = {
         x: Math.round(scr.x + rect.x*scr.width),
         y: Math.round(scr.y + rect.y*scr.height),
@@ -27,6 +28,21 @@ function move_window(rect) {
         height: Math.round(scr.height * rect.height),
     };
     Window.focusedWindow().setFrame(r);
+}
+
+function move_window_to_next_screen() {
+    var currW = Window.focusedWindow();
+    var cwFrame = currW.frame();
+    var currScreen = currW.screen();
+    var nextScreen = currScreen.next();
+    var currScreenSize = currScreen.visibleFrameInRectangle();
+    var relative = {
+        x: (cwFrame.x - currScreenSize.x) / currScreenSize.width,
+        y: (cwFrame.y - currScreenSize.y) / currScreenSize.height,
+        width: cwFrame.width / currScreenSize.width,
+        height: cwFrame.height / currScreenSize.height,
+    };
+    move_window(relative, nextScreen);
 }
 
 /* Support a prefix key with multiple suffix keys */
@@ -49,8 +65,10 @@ PrefixKey.prototype.enableSuffixes = function () {
             that.disableSuffixes();
             x.cb();
             that.modal.close();
+            Phoenix.reload();
         });
         h.enable();
+        that.handlers.push(h);
     });
 };
 PrefixKey.prototype.disableSuffixes = function () {
@@ -65,7 +83,8 @@ PrefixKey.prototype.addSuffix = function (key, modifiers, cb) {
 
 /* Window handling prefix key */
 
-var wPrefix = new PrefixKey('space', ['alt'], "h - Left Half\nl - Right Half\ng - Wide Center\nm - Max\nesc - Abort");
+var wPrefix = new PrefixKey('space', ['ctrl', 'alt', 'cmd'],
+    "h - Left Half\nl - Right Half\ng - Wide Center\nm - Max\no - big left\np - big right\ns - next screen\nesc - Abort");
 wPrefix.addSuffix('h', [], function () {
     move_window({x: 0, y: 0, width: 0.5, height: 1.0});
 });
@@ -84,5 +103,39 @@ wPrefix.addSuffix('n', [], function () {
 wPrefix.addSuffix('c', [], function () {
     move_window({x: 0.2, y: 0.2, width: 0.6, height: 0.6});
 });
+wPrefix.addSuffix('o', [], function () {
+    move_window({x: 0, y: 0, width: 0.9, height: 1.0});
+});
+wPrefix.addSuffix('p', [], function () {
+    move_window({x: 0.1, y: 0, width: 0.9, height: 1.0});
+});
+wPrefix.addSuffix('s', [], function () {
+    move_window_to_next_screen();
+});
 wPrefix.addSuffix('escape', [], function () {});
+wPrefix.addSuffix('space', ['ctrl', 'alt', 'cmd'], function () {});
 
+/* Window List */
+
+function showWindowList() {
+    Phoenix.log("showWindowList start");
+    var windows = Window.windows();
+    Phoenix.log("" + windows.length + " windows");
+    var s = "Window List:\n\n";
+    windows.forEach(function (w) {
+        s += " " + w.title() + "\n";
+    });
+    var m = new Modal();
+    m.message = s;
+    m.duration = 5;
+    m.show();
+}
+var wListTest = Phoenix.bind('g', ['alt'], showWindowList);
+
+// /* testing */
+// var screens = Screen.screens();
+// screens.forEach(function (scr) {
+//     Phoenix.log("Screen");
+//     var r = scr.visibleFrameInRectangle();
+//     Phoenix.log(JSON.stringify(r));
+// });
